431711183241f0b44caa4441990137e10204fdfc
Andreas Knöpfle <andreas.knoepfle@gmail.com>
2015-06-15 18:08:48 +0200

    worked on ontology mappings converter

--- core/src/main/scala/org/dbpedia/extraction/mappings/MappingOntology.scala
+++ core/src/main/scala/org/dbpedia/extraction/mappings/MappingOntology.scala
@@ -2,7 +2,77 @@ package org.dbpedia.extraction.mappings
 
 import java.io.File
 
-class MappingOntology(name: String, file: File) {
+import org.semanticweb.owlapi.apibinding.OWLManager
+import org.semanticweb.owlapi.model._
 
+import scala.collection.mutable.HashMap
+
+
+class TemplateMappingParameter(mapToClass: OWLClass,
+                               correspondingClass: OWLClass,
+                               correspondingProperty: OWLProperty)
+
+class ConditionMappingParameter(templateProperty: String,
+                                 operator: String,
+                                 value: String)
+
+object MappingSchemaOntology {
+  val schema_ontology_iri = IRI.create("http://dbpedia.org/mappings/")
+  val manager = OWLManager.createOWLOntologyManager
+  val ontology = manager.createOntology(schema_ontology_iri)
+  val factory = ontology.getOWLOntologyManager().getOWLDataFactory()
+
+  val cache = new HashMap[String,OWLClass]
+
+  def classFromAnchor(anchor: String): OWLClass = {
+    cache.getOrElseUpdate(anchor,factory.getOWLClass(IRI.create(schema_ontology_iri + "#" + anchor)))
+  }
+}
+
+class MappingOntology(name: String, file: File) {
+  val manager = OWLManager.createOWLOntologyManager
+  val ontology_iri = IRI.create("http://dbpedia.org/mappings/" + name)
+  val ontology = manager.createOntology(ontology_iri)
+  val factory = ontology.getOWLOntologyManager().getOWLDataFactory()
+
+  def createTemplateMapping(name: String, params: TemplateMappingParameter) :OWLClass = {
+    val superClasses = List (MappingSchemaOntology.classFromAnchor("DBpediaMapping"),
+      MappingSchemaOntology.classFromAnchor("TemplateMapping"))
+    createClass(name, superClasses)
+  }
+
+  def createConditionalMapping(name: String): OWLClass = {
+    createClass(
+      name,
+      List(MappingSchemaOntology.classFromAnchor("DBpediaMapping"),
+        MappingSchemaOntology.classFromAnchor("ConditionalMapping"))
+    )
+  }
+
+  def createConditionMapping(name: String, conditionalMapping: OWLClass, params: ConditionMappingParameter): OWLClass = {
+    createClass(
+      name,
+      List(conditionalMapping,
+        MappingSchemaOntology.classFromAnchor("TemplateMapping"))
+    )
+  }
+
+  private def createIRIfromString(name: String): IRI = {
+    IRI.create(ontology_iri + "#" + name)
+  }
+
+  private def createClass(name: String, superClasses: List[OWLClass]): OWLClass = {
+    val owl_class = factory.getOWLClass(createIRIfromString(name))
+    manager.addAxiom(ontology, factory.getOWLDeclarationAxiom(owl_class))
+    for ( superClass <- superClasses) {
+      manager.addAxiom(ontology, factory.getOWLSubClassOfAxiom(owl_class, superClass))
+    }
+    owl_class
+  }
+
+  private def addAnnotation(property: OWLAnnotationProperty , subject: OWLAnnotationSubject ,  value: OWLAnnotationValue): Unit = {
+    manager.addAxiom(ontology, factory.getOWLAnnotationAssertionAxiom(property, subject, value))
+  }
 
 }
+
--- core/src/main/scala/org/dbpedia/extraction/util/MappingOwlConverter.scala
+++ core/src/main/scala/org/dbpedia/extraction/util/MappingOwlConverter.scala
@@ -2,31 +2,47 @@ package org.dbpedia.extraction.util
 
 import org.dbpedia.extraction.mappings.MappingOntology
 import org.dbpedia.extraction.wikiparser.{TemplateNode, Node, PageNode}
+import org.semanticweb.owlapi.model.{OWLClass, OWLProperty}
 
 
 class MappingOwlConverter(page: PageNode, node: Node, mappingOntology: MappingOntology) {
   val tnode = node.asInstanceOf[TemplateNode]
   val name = page.title.decoded
 
-  def convertTemplateMapping(tnode: TemplateNode): Unit = {
-    val test = tnode.property("mapToClass")
-    test.hashCode()
+  def convertTemplateMapping(tnode: TemplateNode, name: String): Unit = {
+    val mapToClass = fetchOntologyClass(tnode,"mapToClass")
+    val correspondingClass = fetchOntologyClass(tnode,"correspondingClass")
+    val correspondingProperty = fetchOntologyProperty(tnode,"correspondingProperty")
+    val templateMapping = mappingOntology.createTemplateMapping(name, mapToClass, correspondingClass, correspondingProperty)
+    extractPropertyMappings(tnode, templateMapping)
   }
 
-  def convertConditionalMapping(tnode: TemplateNode): Unit = {
+  def convertConditionalMapping(tnode: TemplateNode, name: String): Unit = {
+
+  }
+
+  def extractPropertyMappings(tnode: TemplateNode, mapping: OWLClass ): Unit = {
+
+  }
+
+  def fetchOntologyClass(tnode: TemplateNode, attribute: String): OWLClass = {
+
+  }
+
+  def fetchOntologyProperty(tnode: TemplateNode, attribute: String): OWLProperty = {
 
   }
 
   def convert: Unit = {
     tnode.title.decoded match {
       case "TemplateMapping" => {
-        convertTemplateMapping(tnode)
+        convertTemplateMapping(tnode,name)
       }
       case "TableMapping" => {
         // ignore this because it is used only one and is also deprecated
       }
       case "ConditionalMapping" => {
-        convertConditionalMapping(tnode)
+        convertConditionalMapping(tnode,name)
       }
       case _ => throw new scala.IllegalArgumentException("Unknown mapping element " + tnode.title.decoded)
     }
